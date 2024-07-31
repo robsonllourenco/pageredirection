3497b7474e9a076e1815e41025c1d6f03c414d61
Robson Lourenço <125693025+robsonllourenco@users.noreply.github.com>
2024-07-31 11:35:51 -0300

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const redirectURL = "https://euquerobrindesgratis.blogspot.com/";  // Substitua pelo link desejado
+    const redirectDelay = 1000;  // Tempo de espera em milissegundos (3000 ms = 3 segundos)
+
+    setTimeout(function() {
+        window.location.href = redirectURL;
+    }, redirectDelay);
+});
